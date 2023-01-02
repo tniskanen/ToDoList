27d04e8d9a6029d480d80372434cedbf706ec3cb
Ty Niskanen <niskanenty24@gmail.com>
2023-01-02 11:23:52 -0800

    intial commit

---
+++ bonus/bonus2.1.py
@@ -0,0 +1,9 @@
+"""
+password = input("Enter password: ")
+
+while password != "pass123":
+    print("Password Incorrect")
+    password = input("Enter password: ")
+
+print("Password correct")
+"""
